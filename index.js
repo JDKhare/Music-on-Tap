bb24be7e80f29bbd896158d8de5ffd4f51e85c51
JDKhare <45893601+JDKhare@users.noreply.github.com>
2019-12-26 00:08:27 +0530

    improved performance

--- index.js
+++ index.js
@@ -22,6 +22,9 @@ window.addEventListener('load', () => {
         visual.appendChild(bubble);
         bubble.style.backgroundColor = colors[index];
         bubble.style.animation = "jump 1s ease";
+        bubble.addEventListener('animationend', function(){
+            visual.removeChild(this);
+        });
     }; 
 });
 
