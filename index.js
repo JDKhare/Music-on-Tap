6ea39945a638892d079cc51253b7b81748509d3b
JDKhare <45893601+JDKhare@users.noreply.github.com>
2019-12-26 00:00:14 +0530

    Matched color of bubbles with pad

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ window.addEventListener('load', () => {
     const pads = document.querySelectorAll(".pads div");
     const visual = document.querySelector(".visual");
     const colors = [
-        "#f3ffff",
-        "#000fff",
-        "#000fff",
-        "#000fff",
-        "#000fff",
-        "#000fff"
+        "rgb(255, 223, 40)",
+        "rgb(93, 243, 34)",
+        "rgb(243, 100, 34)",
+        "rgb(34, 125, 243)",
+        "rgb(142, 34, 243)",
+        "rgb(243, 34, 173)"
     ];
     pads.forEach((colorpad, index) => {
         colorpad.addEventListener('click', function () {
