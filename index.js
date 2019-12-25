dfa533d02644c443addef038be78bf02a0cb3958
JDKhare <45893601+JDKhare@users.noreply.github.com>
2019-12-25 23:38:59 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+window.addEventListener('load', () => {
+    const audiotracks = document.querySelectorAll(".sounds");
+    const pads = document.querySelectorAll(".pads div");
+    const visual = document.querySelector(".visual");
+    const colors = [
+        "#f3ffff",
+        "#000fff",
+        "#000fff",
+        "#000fff",
+        "#000fff",
+        "#000fff"
+    ];
+    pads.forEach((colorpad, index) => {
+        colorpad.addEventListener('click', function () {
+            audiotracks[index].currentTime = 0;
+            audiotracks[index].play();
+            createBubbles(index);
+        });
+    });
+    const createBubbles = (index) => {
+        const bubble = document.createElement("div");
+        visual.appendchild(bubble);
+        bubble.style.backgroundColor = colors[index];
+        bubble.style.animation = 'jump 1s ease';
+    }; 
+});
+
